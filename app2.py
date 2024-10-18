165fffff7224669fc89f3700172862b10288df28
kenovenas <136008924+kenovenas@users.noreply.github.com>
2024-10-18 20:32:41 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,24 +1,15 @@
-import os
-from flask import Flask, request, jsonify, render_template_string
+from flask import Flask, request, jsonify
 import secrets
 import time
 
 app = Flask(__name__)
-application = app  # Para compatibilidade com ambientes como o Render ou AWS
+application = app
 
-# Armazenamento para chave, timestamp e usuários permitidos
+# Armazenamento para chave e seu timestamp
 key_data = {
     "key": None,
     "timestamp": None
 }
-
-# Usuários permitidos
-allowed_users = {
-    "pstfr", "emda", "wndrsn", "thglm", "emrsnc", "cslxnd", 
-    "wlsn", "edrd", "vttb", "tmmz", "wltr", "crtntt", 
-    "wndrsn", "rcrd", "ndrtx", "vttbt", "mrn", "rflcr", 
-    "cnt", "wbss", "zr1", "nbsbt"
-}  # Adicione os usuários permitidos aqui
 
 # Função para gerar uma chave aleatória
 def generate_key():
@@ -33,169 +24,101 @@ def is_key_valid():
             return True
     return False
 
-# Rota da página de login
 @app.route('/')
-def login():
-    return '''
+def home():
+    if not is_key_valid():
+        key_data["key"] = generate_key()
+        key_data["timestamp"] = time.time()
+    return f'''
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Login</title>
+        <title>Access Key</title>
         <style>
-            body {
+            body {{
                 display: flex;
                 justify-content: center;
                 align-items: center;
                 height: 100vh;
                 margin: 0;
-                background-color: #f4f4f9;
-            }
-            .login-container {
+                position: relative;
+                flex-direction: column;
+            }}
+            .content {{
                 text-align: center;
-                padding: 20px;
-                border-radius: 10px;
-                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                background-color: white;
-                width: 300px;
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-            }
-            .login-container h1 {
-                margin-bottom: 20px;
-            }
-            .login-container form {
-                display: flex;
-                flex-direction: column;
-                width: 100%;
-            }
-            .login-container input {
-                padding: 10px;
-                margin-bottom: 10px;
-                width: 100%;
-                border: 1px solid #ccc;
-                border-radius: 5px;
-            }
-            .login-container button {
-                padding: 10px 20px;
-                background-color: #0088cc;
-                color: white;
-                border: none;
-                border-radius: 5px;
-                cursor: pointer;
-                width: 100%;
-            }
-            .login-container button:hover {
-                background-color: #005f99;
-            }
-            .contact {
                 margin-top: 20px;
-            }
-            .author-link {
-                color: #0088cc;
-                text-decoration: none;
-                font-weight: bold;
-            }
-            .telegram-group {
-                margin-top: 10px;
-            }
-            .telegram-group a {
+            }}
+            .author {{
+                position: absolute;
+                top: 10px;
+                left: 10px;
+                color: #000;
+                font-size: 18px;
+            }}
+            .banner-telegram {{
+                position: absolute;
+                top: 10px;
+                right: 10px;
+                background-color: #0088cc;
+                padding: 10px;
+                border-radius: 5px;
+                box-shadow: 0 2px 4px rgba(0,0,0,0.2);
+            }}
+            .banner-telegram a {{
                 color: #ffcc00;
                 text-decoration: none;
                 font-weight: bold;
-            }
+            }}
+            .ad-banner {{
+                width: 728px;
+                height: 90px;
+                background-color: #f4f4f4;
+                padding: 10px;
+                text-align: center;
+                position: fixed;
+                bottom: 0;
+                box-shadow: 0 -2px 4px rgba(0,0,0,0.2);
+            }}
         </style>
     </head>
     <body>
-        <div class="login-container">
-            <h1>Login</h1>
-            <form action="/generate" method="post">
-                <input type="text" id="username" name="username" placeholder="Usuário" required><br>
-                <button type="submit">Login</button>
-            </form>
-            <div class="contact">
-                <p>Para acessar entre em contato:</p>
-                <a class="author-link" href="https://t.me/Keno_venas" target="_blank">Keno Venas</a>
-            </div>
-            <div class="telegram-group">
-                <p>Grupo do Telegram:</p>
-                <a href="https://t.me/+Mns6IsONSxliZDkx" target="_blank">Crypto Faucets</a>
-            </div>
+        <div class="author">Autor = Keno Venas</div>
+        <div class="banner-telegram">
+            <a href="https://t.me/+Mns6IsONSxliZDkx" target="_blank">Grupo do Telegram</a>
         </div>
+        <div class="content">
+            <h1>Access Key</h1>
+            <p>{key_data["key"]}</p>
+        </div>
+
+        <!-- Script da Hydro -->
+        <script id="hydro_config" type="text/javascript">
+            window.Hydro_tagId = "ab51bfd4-d078-4c04-a17b-ccfcfe865175";
+        </script>
+        <script id="hydro_script" src="https://track.hydro.online/"></script>
+
+
+        <!-- anuncios -->
+        <div class="ad-banner">
+            <script type="text/javascript">
+                atOptions = {{
+                    'key' : '78713e6d4e36d5a549e9864674183de6',
+                    'format' : 'iframe',
+                    'height' : 90,
+                    'width' : 728,
+                    'params' : {{}}
+                }};
+            </script>
+            <script type="text/javascript" src="//spiceoptimistic.com/78713e6d4e36d5a549e9864674183de6/invoke.js"></script>
+        </div>
+<script type='text/javascript' src='//spiceoptimistic.com/1c/66/88/1c668878f3f644b95a54de17911c2ff5.js'></script>
+       
     </body>
     </html>
     '''
 
-# Rota para geração de chave de acesso
-@app.route('/generate', methods=['POST'])
-def generate():
-    username = request.form.get('username')
-    if username in allowed_users:  # Verifica se o usuário está na lista permitida
-        if not is_key_valid():
-            key_data["key"] = generate_key()
-            key_data["timestamp"] = time.time()
-        return render_template_string(f'''
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Access Key</title>
-            <style>
-                body {{
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100vh;
-                    margin: 0;
-                    position: relative;
-                    flex-direction: column;
-                }}
-                .content {{
-                    text-align: center;
-                    margin-top: 20px;
-                }}
-                .author {{
-                    position: absolute;
-                    top: 10px;
-                    left: 10px;
-                    color: #000;
-                    font-size: 18px;
-                }}
-                .banner-telegram {{
-                    position: absolute;
-                    top: 10px;
-                    right: 10px;
-                    background-color: #0088cc;
-                    padding: 10px;
-                    border-radius: 5px;
-                    box-shadow: 0 2px 4px rgba(0,0,0,0.2);
-                }}
-                .banner-telegram a {{
-                    color: #ffcc00;
-                    text-decoration: none;
-                    font-weight: bold;
-                }}
-            </style>
-        </head>
-        <body>
-            <div class="author">Autor = Keno Venas</div>
-            <div class="banner-telegram">
-                <a href="https://t.me/+Mns6IsONSxliZDkx" target="_blank">Grupo do Telegram</a>
-            </div>
-            <div class="content">
-                <h1>Access Key</h1>
-                <p>{key_data["key"]}</p>
-            </div>
-        </body>
-        </html>
-        ''')
-    else:
-        return "Acesso negado", 401
-
-# Rota para validação da chave
 @app.route('/validate', methods=['POST'])
 def validate_key():
     data = request.get_json()
@@ -204,7 +127,5 @@ def validate_key():
             return jsonify({"valid": True}), 200
     return jsonify({"valid": False}), 401
 
-# Inicializa o servidor, ajustando a porta para o ambiente de produção
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
